f4fa493fc7f689d29493add0506a7af02138ae84
varshitha001 <varshitharani17@gmail.com>
2026-03-26 09:57:22 +0530

    small alert changes

--- services/db_service.py
+++ services/db_service.py
@@ -1,6 +1,8 @@
 from models.user_model import User
 from models.transaction_model import Transaction
 from database.db import db
+from services.sns_service import send_alert
+from services.sqs_service import send_to_queue
 
 def get_balance(name):
     user = User.query.filter_by(name=name).first()
@@ -12,6 +14,9 @@ def get_balance(name):
 def deposit_money(name, amount):
     user = User.query.filter_by(name=name).first()
 
+    if not user:
+        return {"msg": "User not found"}
+
     user.balance += amount
 
     txn = Transaction(
@@ -22,19 +27,28 @@ def deposit_money(name, amount):
     )
 
     db.session.add(txn)
+
+    # 📩 SQS LOG
+    send_to_queue(f"{name} deposited ₹{amount}")
+
     db.session.commit()
 
     return {"msg": "Deposit successful", "balance": user.balance}
 
-
 def withdraw_money(name, amount):
     user = User.query.filter_by(name=name).first()
 
+    if not user:
+        return {"msg": "User not found"}
+
+    # ✅ Rule check
     if user.balance - amount < 1000:
         return {"msg": "Minimum balance should be ₹1000"}
 
+    # ✅ Deduct balance
     user.balance -= amount
 
+    # ✅ Create transaction
     txn = Transaction(
         user_id=user.id,
         amount=amount,
@@ -43,9 +57,19 @@ def withdraw_money(name, amount):
     )
 
     db.session.add(txn)
+
+    # 🔔 SNS ALERT (ONLY FOR HIGH AMOUNT)
+    if amount > 10000:
+        send_alert(f"⚠ High withdrawal of ₹{amount} by {name}")
+
+    # 📩 SQS MESSAGE (FOR EVERY TRANSACTION)
+    send_to_queue(f"{name} withdrew ₹{amount}")
+
+    # ✅ Save to DB
     db.session.commit()
 
     return {"msg": "Withdraw successful", "balance": user.balance}
+
 
 
 def get_transactions(name):
